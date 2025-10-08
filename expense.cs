c346b5fdc8ebda3b85fc4af7d88b50281e90c4b0
4090rt <artem2007yannurow@gmail.com>
2025-10-08 22:08:26 +0500

    Add files via upload

---
+++ expense.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp4
+{
+    internal class expense
+    {
+        public string category { get; set; }
+        public decimal count { get; set; }
+        public DateTime date { get; set; }
+    }
+}
