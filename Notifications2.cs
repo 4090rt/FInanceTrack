da79955109aad07f21d26678d3da7439ea2e3dc6
4090rt <artem2007yannurow@gmail.com>
2025-11-18 21:36:19 +0500

    Add files via upload

---
+++ Notifications2.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp4
+{
+    public class Notifications2
+    {
+
+    }
+
+    public class Notificatoinsbd
+    {
+        private static bool _currentindex = false;
+        private static readonly object _lock = new object();
+
+        public Notificatoinsbd()
+        {
+            Emailbdindex().ConfigureAwait(false);
+        }
+        public async Task<string> Emaildb()
+        {
+            Form3 form = new Form3();
+            string dbPath = form.GetDatabasePath();
+            string Login = GlobalData.CurrentLogin;
+            Notifications2PoolConnect pool = new Notifications2PoolConnect(dbPath);
+            SQLiteConnection connect = null;
+            try
+            {
+               connect = pool.Connect();
+                using (var command = new SQLiteCommand("SELECT Mail FROM Usersss WHERE Login = @L", connect))
+                {
+                    command.Parameters.AddWithValue("@L",Login);
+                    var result = await command.ExecuteScalarAsync().ConfigureAwait(false);
+                    if (result != null)
+                    {
+                        return result.ToString();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Возникло исключение:" + ex.Message);
+                return "";
+            }
+            return "";
+        }
+
+        public async Task Emailbdindex()
+        {
+            Form3 form = new Form3();
+            string dbPath = form.GetDatabasePath();
+            Notifications2PoolConnect pool = new Notifications2PoolConnect(dbPath);
+            SQLiteConnection connect = null;
+            lock (_lock)
+            {
+                if (_currentindex) return;
+                _currentindex = true;
+            }
+
+            connect = pool.Connect();
+            using (var command = new SQLiteCommand("CREATE INDEX IF NOT EXISTS Login_index ON Usersss(Login)", connect))
+            { 
+                await command.ExecuteScalarAsync().ConfigureAwait(false);
+            }
+        }
+
+        public async Task Indexproverka()
+        {
+            Form3 form = new Form3();
+            string dbPath = form.GetDatabasePath();
+            Notifications2PoolConnect pool = new Notifications2PoolConnect(dbPath);
+            SQLiteConnection connect = null;
+            string Login = "fddfd";
+            try
+            {
+                connect = pool.Connect();
+                using (var command = new SQLiteCommand($"EXPLAIN QUERY PLAN SELECT Login FROM Usersss WHERE Login = '{Login}'", connect))
+                {
+                    await command.ExecuteScalarAsync().ConfigureAwait(false);
+                    bool result = command != null;
+
+
+                    MessageBox.Show(result ? $"✅ Индекс '{result.ToString()}' создан успешно!" : "❌ Индекс не создан");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Возникло исключение" + ex.Message);
+            }
+        }
+    }
+}
---
+++ Notifications2Pool.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace WinFormsApp4
+{
+
+    public class Notifications2PoolConnect
+    { 
+        private readonly Stack<SQLiteConnection> _pool = new Stack<SQLiteConnection>();
+        private readonly string _connectionString;
+
+
+        public Notifications2PoolConnect(string DBpath)
+        {
+            _connectionString = $"Data Source={DBpath}";
+        }
+
+        public SQLiteConnection Connect()
+        {
+            lock (_pool)
+            {
+                if (_pool.Count > 0)
+                {
+                    _pool.Pop();
+                }
+            }
+            return DB();
+        }
+
+        public SQLiteConnection DB()
+        { 
+            var connect = new SQLiteConnection(_connectionString);
+            connect.Open();
+            return connect;
+        }
+
+        public void Close(SQLiteConnection connect)
+        {
+            if (connect.State == ConnectionState.Broken)
+            {
+                connect.Dispose();
+                return;
+            }
+            lock (_pool)
+            {
+                _pool.Push(connect);
+            }
+        }
+    }
+}
